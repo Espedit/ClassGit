22a878a7d8a07b71f666119b789a297121cd1580
Espedito <espedito.esposito1.1@gmail.com>
2018-11-28 12:38:00 +0100

    Classuser

---
+++ user.php
@@ -0,0 +1,33 @@
+
+<?php 
+
+
+class user {
+
+
+public $user;
+
+
+public function __construct ($string)
+
+{$this->user=$string;}
+
+public function GetUser ()
+
+{return $this->user;}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>

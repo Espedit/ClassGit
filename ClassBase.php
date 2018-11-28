1fa9ce65b62bcd71fa93b462e7fd59dbc88346c4
Espedito <espedito.esposito1.1@gmail.com>
2018-11-28 12:42:53 +0100

    ClassBase

---
+++ ClassBase.php
@@ -0,0 +1,41 @@
+<?php
+
+class ClassBase
+
+{
+   protected $user;
+
+
+   public function setUser (string $string)
+
+   {$this->user=$string;}
+
+
+public function getUser (): string
+
+
+{return $this->user;}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>

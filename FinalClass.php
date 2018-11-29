f990d2711dcb65844921cd9892c7999628c45df1
Espedito <espedito.esposito1.1@gmail.com>
2018-11-29 09:24:27 +0100

    Class (method final)

---
+++ FinalClass.php
@@ -0,0 +1,35 @@
+<?php
+
+ class FinalClass
+
+{
+
+   protected $name;
+
+
+   final public function AddName ($text)
+
+   {$this->name=$text;}
+
+final public function GetName ()
+
+{return $this->name;}
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+ ?>

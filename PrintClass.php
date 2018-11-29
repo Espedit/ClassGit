420eca39141d59542d11245333720dd46bdd0f14
Espedito <espedito.esposito1.1@gmail.com>
2018-11-29 09:20:10 +0100

    Abstract-Standard

---
+++ PrintClass.php
@@ -0,0 +1,38 @@
+
+
+<?php
+
+abstract class PrintClass
+
+{
+   protected $print_text;
+
+
+   public function add_whatever ($string)
+
+   {$this->print_text=$string;}
+
+   public function print_whatever ()
+
+   {return $this->print_text;}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>

9f544abf250b80f89de107416ffd055de22723a7
Espedito <espedito.esposito1.1@gmail.com>
2018-11-29 09:25:37 +0100

    Entities

---
+++ Entities.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Entities{
+
+static $propherty='static-propherty';
+
+static function   ViewEntites ()
+
+
+
+{return $entity='Entity';}
+
+
+static function ViewEntities_Propherty ()
+
+{echo self::$propherty;}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
